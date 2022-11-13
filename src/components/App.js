07dd5bd48762184dbbbc57a564887335cb048b4e
Sahil <sahilthakurxyz@gmail.com>
2022-11-13 23:47:02 +0530

    upadte

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-function App(){
-    return(
-       <>
-       <div className="hello">
-          <div> hello world </div>
-       </div>
-       </>
-    );
-
+import React from "react";
+import "../styles/App.css";
+function App() {
+  return (
+    <div className="App">
+      <p>
+        Now i can render any type of React component on the the Dom node by
+        using ReactDOM
+      </p>
+    </div>
+  );
 }
+
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,12 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
 
-const ReactDOM = ReactDOM.createRoot(document.getElementById('root'))
-ReactDOM.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-);
-
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
